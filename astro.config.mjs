92fdf7b3c778c5d3aaa2ceb8e195286a02f17ca8
Timothy Beyer <timothy.beyer@outlook.com>
2025-02-20 14:16:46 +0100

    updated astro.config.mjs to address dist issue

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,4 +2,7 @@
 import { defineConfig } from 'astro/config';
 
 // https://astro.build/config
-export default defineConfig({});
+export default defineConfig({
+  output: 'static', // Explicitly set output to static
+  base: '/', // Set the base URL (if needed)
+});
