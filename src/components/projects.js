8688f9442b84623a35c8b9ffd1d1d3e7fa6116a7
Timothy Beyer <timothy.beyer@outlook.com>
2025-03-11 14:36:40 +0100

    initial fix to background issue in dk mode; added projects

---
+++ src/components/projects.js
@@ -0,0 +1,36 @@
+const projects = [
+  {
+    title: "What’s in a name",
+    description: "Name generator app built with Python and SQLite, using a FastAPI/Uvicorn backend and deployed with Streamlit, Docker and Cloud Run, allowing users to create new, unique names based on simple patterns.",
+    link: "https://name-gen-ui-581282400880.europe-west1.run.app/",
+    github: "https://github.com/TimB808/name-generator-public",
+    image: "/assets/name-gen.png",
+  },
+  {
+    title: "Pot Luck",
+    description: "Website using NLP embedding and K-Means clustering to allow individuals to search for recipes by ingredients, so they can find a dish to make whatever is in the kitchen.",
+    link: "https://potluckimg1-581282400880.europe-west1.run.app/",
+    github: "https://github.com/TimB808/potluck_project",
+    image: "/assets/Potluck.jpg",
+  },
+  {
+    title: "Career Compass (ongoing)",
+    description: "Web app using web scraping, NLP and K-means clustering to allow users to focus their career search by describing their ideal job.",
+    github: "https://github.com/TimB808/job_scraping",
+    image: "/assets/career-compass.jpg",
+  },
+  {
+    title: "Tableau Public",
+    description: "I’m currently learning Tableau by exploring Tableau Public.",
+    link: "http://public.tableau.com/app/profile/tim.beyer6061/",
+    image: "/assets/icons8-tableau-software-480.png",
+  },
+  {
+    title: "Portfolio site with Astro",
+    description: "I’ve been getting to grips with Astro and a little CSS to build this site.",
+    github: "https://github.com/TimB808/astro-portfolio",
+    image: "/assets/Astro.png",
+  },
+];
+
+export default projects;
