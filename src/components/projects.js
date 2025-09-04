aefd38de365185ce1af9f5eecff883a21b1591f2
Timothy Beyer <timothy.beyer@outlook.com>
2025-09-04 14:37:41 +0200

    added climate data and doc chat projects

--- src/components/projects.js
+++ src/components/projects.js
@@ -1,5 +1,19 @@
 const projects = [
   {
+    title: "Document Chat App",
+    description: "App allowing users to upload PDFs and ask questions about the content. It combines a FastAPI backend with LanceDB for vector search, OpenAI embeddings for semantic understanding, and a Streamlit UI for interactive querying.",
+    link: "https://doc-chat-ui-581282400880.europe-west3.run.app/",
+    github: "https://github.com/TimB808/doc-chat-api",
+    image: "/assets/doc-chat.png",
+  },
+  {
+    title: "Climate Data Pipeline",
+    description: "Data pipeline using xarray, pandas, Dask, and a geospatial stack (geopandas, shapely, regionmask) to transform ERA5 climate and World Bank/OWID socioeconomic data into tidy country-year metrics, stored in Parquet and published via Tableau dashboards.",
+    link: "https://public.tableau.com/app/profile/tim.beyer6061/viz/GlobalheatingERA5AnnualTemps/Trend-AnnualTempC",
+    github: "https://github.com/TimB808/climate-resilience-pipeline",
+    image: "/assets/climate-change-2.jpg",
+  },
+  {
     title: "What’s in a name?",
     description: "Name generator app built with Python and SQLite, using a FastAPI/Uvicorn backend and deployed with Streamlit, Docker and Cloud Run, allowing users to create new, unique names based on simple patterns.",
     link: "https://name-gen-ui-581282400880.europe-west1.run.app/",
